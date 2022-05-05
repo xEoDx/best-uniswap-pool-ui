474a68b3a30fa8bfe3dc27dad9fd7e2002784bd9
Victor Alcazar <victor.alcazar@scopely.com>
2022-05-05 18:04:50 +0200

    Round score for a pool

--- src/app/models/pool.ts
+++ src/app/models/pool.ts
@@ -48,7 +48,7 @@ export class Pool {
         if (this.volume <= 0) {
             return 0;
         }
-        return this.feeTier * this.volume / this.tvl;
+        return Math.round(this.feeTier * this.volume / this.tvl);
     }
 
     public get blockChain(): Blockchain {
