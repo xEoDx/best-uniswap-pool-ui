78393d7fbfa60d99b676271dbec314adff1604fe
xEoDx <victor.alcazar.alcazar@gmail.com>
2022-06-09 23:50:32 +0200

    Revert http change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   isLocalEnv: false,
-  apiEndpoint: 'http://poolanalyzer.com/api/v1',
+  apiEndpoint: 'https://www.poolanalyzer.com/api/v1',
   filter: {
     low: ["BTC", "ETH", "USD", "DAI", "MATIC"],
     moderate: ["BTC", "ETH", "USD", "DAI", "MATIC", "MANA", "DOGE", "LINK", "COMP", "SHIB", "APE", "UNI", "DYDX"]
