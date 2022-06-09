ec9fbd21fa110689cb16a9a1b9d2a82a4056d054
xEoDx <victor.alcazar.alcazar@gmail.com>
2022-06-09 23:04:19 +0200

    Update ssl endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   isLocalEnv: false,
-  apiEndpoint: 'https://best-uniswap-pool.herokuapp.com/api/v1',
+  apiEndpoint: 'https://poolanalyzer.com/api/v1',
   filter: {
     low: ["BTC", "ETH", "USD", "DAI", "MATIC"],
     moderate: ["BTC", "ETH", "USD", "DAI", "MATIC", "MANA", "DOGE", "LINK", "COMP", "SHIB", "APE", "UNI", "DYDX"]
