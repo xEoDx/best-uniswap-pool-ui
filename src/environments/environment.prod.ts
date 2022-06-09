b02f69787890eb514c95a3fb620944a966b76d6b
xEoDx <victor.alcazar.alcazar@gmail.com>
2022-06-09 23:45:25 +0200

    Update client endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   isLocalEnv: false,
-  apiEndpoint: 'https://poolanalyzer.com/api/v1',
+  apiEndpoint: 'http://poolanalyzer.com/api/v1',
   filter: {
     low: ["BTC", "ETH", "USD", "DAI", "MATIC"],
     moderate: ["BTC", "ETH", "USD", "DAI", "MATIC", "MANA", "DOGE", "LINK", "COMP", "SHIB", "APE", "UNI", "DYDX"]
